1f742f1701bd55b441365cfdc69b3613edd6a886
spetrosino32 <s.petrosino32@studenti.unisa.it>
2026-07-09 17:53:29 +0200

    inserite le classi model e la pagina di riepilogo viaggio

---
+++ travel_app_02/lib/controllers/riepilogo_viaggio_controller.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_02/models/spesa.dart';
+import 'package:travel_app_02/models/viaggio.dart';
+
+class RiepilogoViaggioController extends ChangeNotifier {
+  final Viaggio trip;
+
+  RiepilogoViaggioController({required this.trip});
+  
+  double get speseTotali {
+    return trip.spese.fold(0, (sum, item) => sum + item.importo);
+  }
+
+  Color get statoViaggioColor {
+    final now = DateTime.now();
+    if (now.isBefore(trip.dataInizio)) {
+      return Colors.red; 
+    } else if (now.isAfter(trip.dataFine)) {
+      return Colors.green; 
+    } else {
+      return Colors.amber; 
+    }
+  }
+
+  Color get speseTotaliColor {
+    if (speseTotali > trip.budgetPrevisto) {
+      return Colors.red; 
+    } else {
+      return Colors.green; 
+    }
+  }
+
+  void aggiungiSpesa(Spesa spesa) {
+    trip.spese.add(spesa);
+    notifyListeners(); 
+  }
+}
